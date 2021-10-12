3a187a14f13897ad574db584995185395143bf31
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2021-10-13 00:58:56 +0500

    Adds in the ability to search people

--- src/const/odata_api.js
+++ src/const/odata_api.js
@@ -1,5 +1,6 @@
 const BASE_URL = 'https://services.odata.org/TripPinRESTierService/';
 
 module.exports = {
-    LIST_PEOPLE: (count) => count > 0 ? BASE_URL + 'People?$top=' + count : BASE_URL + 'People'
+    LIST_PEOPLE: (count) => count > 0 ? BASE_URL + 'People?$top=' + count : BASE_URL + 'People',
+    SEARCH_PEOPLE: `${BASE_URL}People`
 }
